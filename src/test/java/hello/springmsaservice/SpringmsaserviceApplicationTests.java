06c00202f1118040aac6c7478b420ff3ca809072
a2175 <as217512@gmail.com>
2024-12-03 17:32:30 +0900

    init

---
+++ src/test/java/hello/springmsaservice/SpringmsaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmsaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmsaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
